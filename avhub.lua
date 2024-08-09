5bd79407777c7e654e805d57d023865fdd277bf0
vyworn <153512249+vyworn@users.noreply.github.com>
2024-08-09 07:32:19 +0200

    Update avhub.lua

--- avhub.lua
+++ avhub.lua
@@ -1,27 +1,21 @@
 if not game:IsLoaded() then
-	game.Loaded:Wait()
+    game.Loaded:Wait()
 end
 
 task.wait(math.random())
 
 local games = {
-	[33660620] = 'https://raw.githubusercontent.com/vyworn/rng/main/acb.lua', -- ACB
+    [33660620] = 'https://raw.githubusercontent.com/vyworn/rng/main/acb.lua', -- ACB
 }
 
 if games[game.CreatorId] then
-	task.wait(math.random())
-	local function initializeAvhub()
-		if not _G.avhub then
-			loadstring(game:HttpGet(games[game.CreatorId]))()
-		end
-	end
-	
-	if game.CreatorId == 33660620 then
-		repeat
-			initializeAvhub()
-			task.wait(10)
-		until _G.avhub ~= nil
-	else
-		initializeAvhub()
-	end
+    task.wait(math.random())
+    if game.CreatorId == 33660620 then
+        repeat
+            loadstring(game:HttpGet(games[game.CreatorId]))()
+            task.wait(10)
+        until _G.ahKey and _G[_G.ahKey] ~= nil
+    else
+        loadstring(game:HttpGet(games[game.CreatorId]))()
+    end
 end
