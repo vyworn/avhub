a6c16a65ac4a512bcac5ec38c44b52612e340a97
vyworn <153512249+vyworn@users.noreply.github.com>
2024-08-09 05:43:17 +0200

    Update avhub.lua

--- avhub.lua
+++ avhub.lua
@@ -1,3 +1,4 @@
+-- Wait until the game is fully loaded
 if not game:IsLoaded() then
 	game.Loaded:Wait()
 end
@@ -10,12 +11,18 @@ local games = {
 
 if games[game.CreatorId] then
 	task.wait(math.random())
+	local function initializeAvhub()
+		if not _G.avhub then
+			loadstring(game:HttpGet(games[game.CreatorId]))()
+		end
+	end
+	
 	if game.CreatorId == 33660620 then
 		repeat
-			loadstring(game:HttpGet(games[game.CreatorId]))()
+			initializeAvhub()
 			task.wait(10)
-		until getgenv().avhub ~= nil;
+		until _G.avhub ~= nil
 	else
-		loadstring(game:HttpGet(games[game.CreatorId]))()
+		initializeAvhub()
 	end
 end
