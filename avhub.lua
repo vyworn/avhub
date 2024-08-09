5a7b86e8a2a1b9f0956025e07ebb92d1f8b13f46
vyworn <153512249+vyworn@users.noreply.github.com>
2024-08-09 05:36:46 +0200

    Add files via upload

---
+++ avhub.lua
@@ -0,0 +1,21 @@
+if not game:IsLoaded() then
+	game.Loaded:Wait()
+end
+
+task.wait(math.random())
+
+local games = {
+	[33660620] = 'https://raw.githubusercontent.com/vyworn/rng/main/acb.lua', -- ACB
+}
+
+if games[game.CreatorId] then
+	task.wait(math.random())
+	if game.CreatorId == 33660620 then
+		repeat
+			loadstring(game:HttpGet(games[game.CreatorId]))()
+			task.wait(10)
+		until getgenv().avhub ~= nil;
+	else
+		loadstring(game:HttpGet(games[game.CreatorId]))()
+	end
+end
