8812ff56c5a88de7ba56826b19f42f0d9139c00c
vyworn <anthony12052004@gmail.com>
2024-08-09 23:27:46 +0200

    Update

--- avhub.lua
+++ avhub.lua
@@ -8,15 +8,16 @@ local games = {
     [18138547215] = 'https://raw.githubusercontent.com/vyworn/rng/main/acb.lua', -- ACB
 }
 
-if games[game.PlaceId] then
+if not games[game.PlaceId] then
+    warn('Game not supported')
+    games = nil
+    return
+else
     task.wait(math.random())
     if games[game.PlaceId] then
         repeat
             loadstring(game:HttpGet(games[game.PlaceId]))()
-            task.wait(5)
+            task.wait(math.random())
         until _G.ahKey and _G[_G.ahKey] ~= nil
     end
-else
-    games = nil
-    return
 end
