e847d55e3ebe3dd6b013c058305b688747a18553
vyworn <153512249+vyworn@users.noreply.github.com>
2024-08-09 22:23:33 +0200

    Update avhub.lua

--- avhub.lua
+++ avhub.lua
@@ -8,22 +8,17 @@ local games = {
     [18138547215] = 'https://raw.githubusercontent.com/vyworn/rng/main/acb.lua', -- ACB
 }
 
-local name = _G.ahKey .. _G.ahKey
-_G[name] = function()
+if games[game.PlaceId] then
+    task.wait(math.random())
     if games[game.PlaceId] then
-        task.wait(math.random())
-        if games[game.PlaceId] then
-            repeat
-                loadstring(game:HttpGet(games[game.PlaceId]))()
-                task.wait(5)
-            until _G.ahKey and _G[_G.ahKey] ~= nil
-        else
+        repeat
             loadstring(game:HttpGet(games[game.PlaceId]))()
-        end
+            task.wait(5)
+        until _G.ahKey and _G[_G.ahKey] ~= nil
     else
-        games = nil
-        return
+        loadstring(game:HttpGet(games[game.PlaceId]))()
     end
+else
+    games = nil
+    return
 end
-
-_G[name]()
