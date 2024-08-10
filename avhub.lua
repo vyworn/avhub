f134a063be928a1c140195727c54ec9ea59a5456
vyworn <anthony12052004@gmail.com>
2024-08-10 10:17:57 +0200

    Update

--- avhub.lua
+++ avhub.lua
@@ -3,7 +3,7 @@ if not game:IsLoaded() then
 end;
 task.wait(math.random());
 local games = {
-	[18138547215] = "https://raw.githubusercontent.com/vyworn/rng/main/acb.lua"
+	[18138547215] = "https://raw.githubusercontent.com/vyworn/rng/main/acb.lua",
 };
 if not games[game.PlaceId] then
 	warn("Game not supported");
@@ -13,7 +13,7 @@ else
 	task.wait(math.random());
 	if games[game.PlaceId] then
 		repeat
-			(loadstring(game:HttpGet(games[game.PlaceId])))();
+			(loadstring(game:HttpGet(games[game.PlaceId])))()
 			task.wait(math.random());
 		until _G.ahKey and _G[_G.ahKey] ~= nil;
 	end;
