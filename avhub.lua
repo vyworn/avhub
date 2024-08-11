c1f5678138cc2958fa9d5ec90d8c9cbc6f3bebd9
vyworn <anthony12052004@gmail.com>
2024-08-11 02:55:49 +0200

    Update

--- avhub.lua
+++ avhub.lua
@@ -1,9 +1,21 @@
 if not game:IsLoaded() then
 	game.Loaded:Wait();
 end;
+local workspace = game:GetService("Workspace");
+local virtualuser = game:GetService("VirtualUser");
+local player = game.Players.LocalPlayer;
+local function antiAfk()
+	player.Idled:connect(function()
+		virtualuser:Button2Down(Vector2.new(0, 0), workspace.CurrentCamera.CFrame);
+		task.wait(0.5);
+		virtualuser:Button2Up(Vector2.new(0, 0), workspace.CurrentCamera.CFrame);
+	end);
+	print("Anti-AFK enabled");
+end;
+antiAfk();
 task.wait(math.random());
 local games = {
-	[18138547215] = "https://raw.githubusercontent.com/vyworn/rng/main/acb.lua",
+	[18138547215] = "https://raw.githubusercontent.com/vyworn/rng/main/acb.lua"
 };
 if not games[game.PlaceId] then
 	warn("Game not supported");
@@ -13,7 +25,7 @@ else
 	task.wait(math.random());
 	if games[game.PlaceId] then
 		repeat
-			(loadstring(game:HttpGet(games[game.PlaceId])))()
+			(loadstring(game:HttpGet(games[game.PlaceId])))();
 			task.wait(math.random());
 		until _G.ahKey and _G[_G.ahKey] ~= nil;
 	end;
